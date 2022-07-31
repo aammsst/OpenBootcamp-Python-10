f4c28c5cdda4255f7a1d7a650efaef220d4cb58f
Fabricio Navarro <Navarro.fa998@gmail.com>
2022-07-31 00:49:15 -0300

    Corrección (mostrando selección)

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,7 +5,7 @@ window = tkinter.Tk()
 
 # Configuración de pantalla
 window.columnconfigure(0, weight=1)
-window.columnconfigure(0, weight=5)
+window.columnconfigure(0, weight=6)
 
 # Variable de registro de opción marcada
 selected = tkinter.StringVar()
@@ -13,14 +13,21 @@ selected = tkinter.StringVar()
 # función que desmarca la opción marcada
 def reiniciar(evento):
     selected.set(None)
+    t1.config(text='')
+
+# Mostrando opción seleccionada
+def mostrar():
+    t1.config(text="{}".format(selected.get()))
 
 # Elementos
 # RadioButton
-r1 = ttk.Radiobutton(window, text='Sí', value='1', variable=selected)
-r2 = ttk.Radiobutton(window, text='No', value='2', variable=selected)
-r3 = ttk.Radiobutton(window, text='Quizá', value='3', variable=selected)
-r4 = ttk.Radiobutton(window, text='Preguntar más tarde', value='4', variable=selected)
-r5 = ttk.Radiobutton(window, text='Prefiere no responder', value='5', variable=selected)
+r1 = ttk.Radiobutton(window, text='Sí', value='Sí', command=mostrar, variable=selected)
+r2 = ttk.Radiobutton(window, text='No', value='No', command=mostrar, variable=selected)
+r3 = ttk.Radiobutton(window, text='Quizá', value='Quizá', command=mostrar, variable=selected)
+r4 = ttk.Radiobutton(window, text='Preguntar más tarde', value='Preguntar más tarde', command=mostrar, variable=selected)
+r5 = ttk.Radiobutton(window, text='Prefiere no responder', value='Prefiere no responder', command=mostrar, variable=selected)
+# Mostrador de selección
+t1 = ttk.Label(window)
 # Boton de Reinicio
 b1 = ttk.Button(window, text='Reiniciar')
 b1.bind('<Button-1>', reiniciar)
@@ -31,6 +38,7 @@ r2.grid(column = 0, row=1, pady=5, padx=5)
 r3.grid(column = 0, row=2, pady=5, padx=5)
 r4.grid(column = 0, row=3, pady=5, padx=5)
 r5.grid(column = 0, row=4, pady=5, padx=5)
-b1.grid(column = 1, row=5, pady=7, padx=7)
+t1.grid(column = 1, row=5, pady=5, padx=5)
+b1.grid(column = 1, row=6, pady=7, padx=7)
 
 window.mainloop()
